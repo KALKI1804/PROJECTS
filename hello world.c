1ed0878afae5987b1aafb3fb6f2367d5ac8817ef
KALKI1804 <145177757+KALKI1804@users.noreply.github.com>
2023-10-08 18:51:01 +0530

    C:\Users\Lenovo\Downloads\c programming\raghav nargotra\hello world.c

---
+++ hello world.c
@@ -0,0 +1,29 @@
+
+
+int main() {
+    int num1, num2;
+
+    // Input two numbers
+    printf("Enter the first number: ");
+    scanf("%d", &num1);
+    printf("Enter the second number: ");
+    scanf("%d", &num2);
+
+    // Addition
+    int sum = num1 + num2;
+    printf("Sum: %d\n", sum);
+
+    // Subtraction
+    int diff = num1 - num2;
+    printf("Difference: %d\n", diff);
+
+    // Multiplication
+    int product = num1 * num2;
+    printf("Product: %d\n", product);
+
+    // Remainder
+    int remainder = num1 % num2;
+    printf("Remainder: %d\n", remainder);
+
+    return 0;
+}
